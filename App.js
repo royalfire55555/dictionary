241685e1fe8bb2d5c243514ff45400c54d9c5ad5
RoyalFire <iamroyalfire@gmail.com>
2022-05-04 20:54:37 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			text: "",
+			word: "loading...",
+			lexicalCategory: "Loading...",
+			examples: [],
+			definition: "Loading...",
+		};
+	}
+
+	search() {
+		var word = this.state.text;
+		if (word.length > 0) {
+			var url =
+				"https://rupinwhitehatjr.github.io/dictionary/%22" + word + "%22.json";
+
+			return fetch(url).then((data) => {
+				if (data.state == 200) {
+					var apiData = data.json();
+					var wordData = apiData.definitions[0];
+					var definition = wordData.description;
+					var lexicalCategory = wordData.wordType;
+					console.log(definition);
+					console.log(lexicalCategory);
+
+					this.setState({
+						word: word,
+						lexicalCategory: lexicalCategory,
+						definition: definition,
+					});
+				} else {
+					this.setState({
+						word: "loading...",
+						lexicalCategory: "Loading...",
+						examples: [],
+						definition: "Loading...",
+					});
+				}
+			});
+		}
+	}
+
+	render() {
+		return (
+			<View>
+				<Text style={styles.title}>Dictionary</Text>
+				<TextInput
+					style={styles.textInput}
+					onChangeText={(text) => {
+						this.setState({
+							text: text,
+						});
+						this.search();
+					}}
+				/>
+				<Text style={styles.title}>Definition: {this.state.definition}</Text>
+				<Text style={styles.title}>
+					Lexical Category: {this.state.lexicalCategory}
+				</Text>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	textInput: {
+		border: "1px solid",
+		borderRadius: 500,
+		width: 500,
+		height: 100,
+		textAlign: "center",
+		fontSize: 30,
+		fontFamily: "Confortaa-Medium.ttf",
+		margin: "auto",
+		display: "flex",
+		justifyContent: "center",
+	},
+	title: {
+		margin: "auto",
+		fontSize: 50,
+		fontFamily: "Righteous-Regular.ttf",
+		display: "flex",
+		justifyContent: "center",
+	},
 });
